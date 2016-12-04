5ab87b92e6dabe9ba2a134770dc8421ed5f60e16
chung.triniti@gmail.com <johndoe@example.com>
2016-12-04 18:46:28 +0800

    Fix currency call anti pattern Get /currencies Post /currencies

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Spree::Core::Engine.add_routes do
-  post '/currency/set', to: 'currency#set', defaults: { format: :json }, as: :set_currency
-  post '/currency/get', to: 'currency#get', defaults: { format: :json }, as: :get_currency
-
+  post '/currencies', to: 'currency#set', defaults: { format: :json }, as: :set_currency
+  get '/currencies', to: 'currency#get', defaults: { format: :json }, as: :get_currency
+  
   namespace :admin do
     resources :products do
       resources :prices, only: [:index, :create]
