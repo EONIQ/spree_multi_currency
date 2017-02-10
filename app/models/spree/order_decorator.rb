a00eff9821fbbd90010814e2a764d5d15793e3d5
chung.triniti@gmail.com <johndoe@example.com>
2017-02-10 23:22:50 +0800

    Fix

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -3,7 +3,11 @@ Spree::Order.class_eval do
     self[:currency] || self.currency_default
   end
 
-  def currency_default
+  def self.currency_default
   	Spree::Store.current_store ? Spree::Store.current_store.default_currency : nil || Spree::Config[:currency]
+  end
+
+  def currency_default
+    self.class.currency_default
   end
 end
