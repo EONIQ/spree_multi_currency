1ef07cab999750365cbcbdf3a3c785debaf143a1
charleswong28 <johndoe@example.com>
2018-03-01 13:06:06 +0800

    Fix session missing problem

--- lib/spree/core/controller_helpers/store.rb
+++ lib/spree/core/controller_helpers/store.rb
@@ -5,7 +5,9 @@ module Spree
         def current_currency
           # ensure session currency is supported
           #
-          if session.key?(:currency) && supported_currencies.map(&:iso_code).include?(session[:currency])
+          if defined?(session) && session.key?(:currency) && 
+            supported_currencies.map(&:iso_code).include?(session[:currency])
+            
             session[:currency]
           else
             Spree::Config[:currency]
